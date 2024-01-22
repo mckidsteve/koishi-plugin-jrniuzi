3e7e64a55847aa5ff01e0ab55dd3637413596a53
mckidsteve <734942701@qq.com>
2024-01-22 13:35:32 +0800

    create program
    hello koishi

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,9 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+   ctx.on('message' , (session) => {
+        if (session.content === 'hello'){
+            session.send('hello world')
+        }
+    })
 }
