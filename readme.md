7cd1301e17e7d0a8efdf6a6df21cf375e283eceb
mckidsteve <734942701@qq.com>
2024-01-22 15:25:14 +0800

    commit version 0.0.2
    complete jrwordle game (use local lib but cv from git repo (MaikoTan) )

--- readme.md
+++ readme.md
@@ -2,4 +2,11 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-jrniuzi?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-jrniuzi)
 
+# niuzigame机器人插件
 a plugin for niuzigame dev by mxk
+
+## 开发计划
+- [√] wordle
+
+## 许可证
+[MIT](https://github.com/mckidsteve/koishi-plugin-jrniuzi/tree/master/LICENSE)
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,52 @@
 import { Context, Schema } from 'koishi'
+import {defineVariation} from './wordle_core/core'
+
+import wordlist from './data/wordlist.json'
 
 export const name = 'jrniuzi'
+export const usage = 'a niuzi game dev by mckidsteve'
 
 export interface Config {}
-
 export const Config: Schema<Config> = Schema.object({})
 
-export function apply(ctx: Context) {
-   ctx.on('message' , (session) => {
-        if (session.content === 'hello'){
-            session.send('hello world')
-        }
-    })
+function getRandomWord() {
+    return wordlist[Math.floor(Math.random() * wordlist.length)];
 }
+
+export default defineVariation<Config>({
+    name: 'koishi-plugin-jrniuzi',
+    command: 'jrwordle',
+    Config: Schema.intersect([
+        Schema.intersect([
+        Schema.object({
+            fallbackToRandom: Schema.boolean().default(true),
+        }),
+        Schema.union([
+            Schema.object({
+            fallbackToRandom: Schema.const(true),
+            timeout: Schema.number().default(5000),
+            }),
+            Schema.object({
+            fallbackToRandom: Schema.const(false),
+            }),
+        ]),
+        ]),
+    ]),
+    validWords: wordlist.map((word: string) => word.split('')),
+    //   init(command, ctx) {
+    //     command.option('random', '-r')
+    //     },
+    async getCurrentWord({ session }, { ctx, config }) {
+        await session.send('请输入一个5个字母的单词')
+        return getRandomWord().split('')
+    }
+})
+
+
+// export function apply(ctx: Context) {
+//    ctx.on('message' , (session) => {
+//         if (session.content === 'hello wordle'){
+//             session.send(getRandomWord().split(''))
+//         }
+//     })
+// }
