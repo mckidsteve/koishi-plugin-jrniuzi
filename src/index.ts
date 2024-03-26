73ec29693d1698617d3609c65d41b2f5aadc221a
mckidsteve <734942701@qq.com>
2024-03-26 14:28:20 +0800

    last update#20240328

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,73 @@
-import { Context, Schema } from 'koishi'
-import {defineVariation} from './wordle_core/core'
+import { Context, Schema , Logger } from 'koishi'
 
-import wordlist from './data/wordlist.json'
+import * as jrw from './wordle_core/wordle'
+import * as jrcd from './jrcd/jrcd'
 
 export const name = 'jrniuzi'
-export const usage = 'a niuzi game dev by mckidsteve'
-
-export interface Config {
-    GuessWordLength: number
+export const usage = `
+<style>
+html , body{
+    width : 100%;
+    height : 100%;
+    display : flex;
+    background : #000;
+}
+svg {
+  width: 100%;
+  height: 100px;
+  margin: auto;
+}
+svg text {
+  text-transform: uppercase;
+  animation: stroke 5s infinite alternate;
+  letter-spacing: 10px;
+  font-size: 90px;
+}
+@keyframes stroke {
+  0% {
+    fill: rgba(72, 138, 20, 0);
+    stroke: rgba(54, 95, 160, 1);
+    stroke-dashoffset: 25%;
+    stroke-dasharray: 0 50%;
+    stroke-width: 0.8;
+  }
+  50% {
+    fill: rgba(72, 138, 20, 0);
+    stroke: rgba(54, 95, 160, 1);
+    stroke-width: 1.2;
+  }
+  70% {
+    fill: rgba(72, 138, 20, 0);
+    stroke: rgba(54, 95, 160, 1);
+    stroke-width: 1.5;
+  }
+  90%,
+  100% {
+    fill: rgba(72, 138, 204, 1);
+    stroke: rgba(54, 95, 160, 0);
+    stroke-dashoffset: -25%;
+    stroke-dasharray: 50% 0;
+    stroke-width: 0;
+  }
 }
 
-function getRandomWord() {
-    return wordlist[Math.floor(Math.random() * wordlist.length)];
+</style>
+<svg viewBox="400 0 400 200">
+  <text x="0" y="70%"> Koishi-Plugin-jrniuzi </text>
+</svg>
+
+# a niuzi game dev by mckidsteve
+`
+
+
+interface PluginEnableConfig{
+  enable : boolean;
 }
 
-export default defineVariation<Config>({
-    name: 'koishi-plugin-jrniuzi',
-    command: 'jrwordle',
-    Config: Schema.intersect([
-        Schema.intersect([
-            Schema.object({
-                GuessWordLength: Schema.number().default(5).description('自定义猜测长度'),
-            }),
-        ]),
-    ]),
-    guessCount: 5,
-    validWords: wordlist.map((word: string) => word.split('')),
-    async getCurrentWord({ session }, { config }) {
-        // const GuessWordLength = config.GuessWordLength
-        await session.send('请输入一个5个字母的单词')
-        const WordInfo = getRandomWord().split('')
-        return WordInfo
-    }
-})
+interface myConfig{
+    jrwordle?: jrw.myConfig & PluginEnableConfig
+    jrcd?: jrcd.myConfig & PluginEnableConfig
+}
 
+function pluginLoad<T>(schema: Schema<T>): Schema<T & PluginEnableConfig> {
+    
---
+++ src/wordle_core/wordle.ts
@@ -0,0 +1,36 @@
+import { Context, Schema } from 'koishi'
+import {defineVariation} from './wordle_core/core'
+
+import wordlist from '../data/wordlist.json'
+
+export const name = 'jrniuzi'
+export const usage = 'a niuzi game dev by mckidsteve'
+
+export interface myConfig {
+    GuessWordLength: number
+}
+
+function getRandomWord() {
+    return wordlist[Math.floor(Math.random() * wordlist.length)];
+}
+
+export default defineVariation<myConfig>({
+    name: 'koishi-plugin-jrniuzi',
+    command: 'jrwordle',
+    Config: Schema.intersect([
+        Schema.intersect([
+            Schema.object({
+                GuessWordLength: Schema.number().default(5).description('自定义猜测长度'),
+            }),
+        ]),
+    ]),
+    guessCount: 5,
+    validWords: wordlist.map((word: string) => word.split('')),
+    async getCurrentWord({ session }, { config }) {
+        // const GuessWordLength = config.GuessWordLength
+        await session.send('请输入一个5个字母的单词')
+        const WordInfo = getRandomWord().split('')
+        return WordInfo
+    }
+})
+
